47dfa25e1be24c15c7e267c4bd19cce538480100
unknown <Lelise.Demissie@Kifiya.com>
2023-03-27 09:57:23 +0300

    config mgt

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,8 @@
-# Installs puppet-lint, version 2.1.1
+package { 'python3-pip':
+  ensure => installed,
+}
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'gem',
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
 }
